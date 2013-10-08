3714a19ce651f007aaea749d456398dbc8196c45
Daisuke Yamashita <dddaisuke@gmail.com>
2013-10-08 21:46:40 +0900

    rails new sginote -d mysql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sginote::Application.config.secret_key_base = '34458577a1dd555c362ade574510c2dc163fb202d6b1ed3c52bbded898fb04236453f84706069a086a95c2a8563c1d2a24684c4407f6f5a59c565e9026e3069e'

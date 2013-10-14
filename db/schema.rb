71238c96a7a8763c6a1939e665c491ffb5f8442d
Daisuke Yamashita <dddaisuke@gmail.com>
2013-10-15 08:16:23 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131014231231) do
+ActiveRecord::Schema.define(version: 20131014231444) do
 
   create_table "mercury_images", force: true do |t|
     t.string   "image_file_name"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20131014231231) do
     t.string   "title"
     t.integer  "user_id"
     t.integer  "notes_count"
+    t.datetime "published_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "notes", force: true do |t|
+    t.string   "title"
+    t.string   "body"
+    t.integer  "user_id"
     t.datetime "published_at"
     t.datetime "created_at"
     t.datetime "updated_at"

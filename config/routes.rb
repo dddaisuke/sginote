bd1e33f95951662089ba51dca2ed18481dba705a
Daisuke Yamashita <dddaisuke@gmail.com>
2013-10-15 07:29:50 +0900

    rails g mercury:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Sginote::Application.routes.draw do
+  mount Mercury::Engine => '/'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

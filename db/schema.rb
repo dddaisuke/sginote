bedd1a462bee6088ae42ecdca7dd068905ddfbf9
Daisuke Yamashita <dddaisuke@gmail.com>
2013-11-04 21:23:03 +0900

    userモデルの追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131014231444) do
+ActiveRecord::Schema.define(version: 20131026040115) do
 
   create_table "mercury_images", force: true do |t|
     t.string   "image_file_name"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 20131014231444) do
     t.text     "body"
     t.integer  "user_id"
     t.datetime "published_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
